a73b5098429fc0573028863daa4b9693fab6416d
Nan Jiang <jiangnan.hugo@gmail.com>
2018-07-10 11:14:45 +0800

    update2

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 echo "builing envoronment..."
-pip install tensorflow_gpu==1.4
-pip install pandas sklearn nltk
-python -c "import nltk; nltk.download()"
-python builder.py
+mkdir data
+cd data && wget https://nlp.stanford.edu/data/glove.840B.300d.zip
+cd data && unzip glove.840B.300d.zip
+python3 -m pip install tensorflow_gpu==1.4
+python3 -m pip install pandas sklearn nltk
+python3 -c "import nltk; nltk.download()"
+python3 builder.py
 echo "now goto the att_rnn folder"
